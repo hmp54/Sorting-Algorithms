0c06b33d7be7b9cf406919d5393ba0d673685150
Helen Pewther <hmp54@georgetown.edu>
2022-01-13 23:15:21 -0800

    Create quickSort.cpp

---
+++ quickSort.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std; 
+
+
+int main(){
+    int reverseInput[10] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    int bestCase[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int averageCase[10] = {3, 4, 7, 8, 9, 1, 2, 5, 10, 6}; 
+}
